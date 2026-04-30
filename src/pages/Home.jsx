51a25b40cf58a0e7dbf9d1c46fe85be7c74e5fbb
fronmen-hgmn <azadyusibov555@gmail.com>
2026-04-30 19:03:24 +0400

    ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <div className="grid sm:grid-cols- lg:grid-cols-0">
             <main className="position  z-index  ">
-                <h1 className="text-8xl font-bold ">Space Exploration</h1>
+                <h1 className="text-4xl sm:text-3xl md:text-6xl lg:text-8xl font-bold">Space Exploration</h1>
                 <p className="mt-4 text-2xl">Discover planets, stars, and galaxies beyond imagination</p>
                 <h1 className="mt-5">If you don't know where to start, click the random button.</h1>
                 <button className="button" onClick={()=>navigate("/stars")}>Start your journey <FontAwesomeIcon icon={faArrowRight} /></button>
